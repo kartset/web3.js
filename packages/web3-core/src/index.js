ae7f250e12136a63a1040fb0ce2030f5d6967a06
frankiebee <frankie.diamond@gmail.com>
2020-08-07 01:21:47 -1000

    Use es6 class syntax

--- packages/web3-core/src/index.js
+++ packages/web3-core/src/index.js
@@ -20,68 +20,60 @@
  * @date 2017
  */
 
-const requestManager = require("web3-core-requestmanager");
+const { Manager, BatchManager } = require("web3-core-requestmanager");
 const extend = require("./extend");
 
-const packageInit = (pkg, args) => {
-    args = Array.prototype.slice.call(args);
 
-    if (!pkg) {
-        throw new Error('You need to instantiate using the "new" keyword.');
-    }
 
-    // make property of pkg._provider, which can properly set providers
-    Object.defineProperty(pkg, 'currentProvider', {
-        get: () => {
-            return pkg._provider;
-        },
-        set: (value) => {
-            return pkg.setProvider(value);
-        },
-        enumerable: true,
-        configurable: true
-    });
 
-    // inherit from parent package or create a new RequestManager
+
+
+class Core {
+  constructor (...args) {
+    // sets _requestmanager etc
     if (args[0] && args[0]._requestManager) {
-        pkg._requestManager = args[0]._requestManager;
+        this._requestManager = args[0]._requestManager;
     } else {
-        pkg._requestManager = new requestManager.Manager(args[0], args[1]);
+        this._requestManager = Manager(args[0], args[1]);
     }
 
     // add givenProvider
-    pkg.givenProvider = requestManager.Manager.givenProvider;
-    pkg.providers = requestManager.Manager.providers;
+    this.givenProvider = Manager.givenProvider;
+    this.providers = Manager.providers;
 
-    pkg._provider = pkg._requestManager.provider;
-
-    // add SETPROVIDER function (don't overwrite if already existing)
-    if (!pkg.setProvider) {
-        pkg.setProvider = (provider, net) => {
-            pkg._requestManager.setProvider(provider, net);
-            pkg._provider = pkg._requestManager.provider;
-            return true;
-        };
-    }
-
-    pkg.setRequestManager = (manager) => {
-        pkg._requestManager = manager;
-        pkg._provider = manager.provider;
-    };
+    this._provider = this._requestManager.provider;
 
     // attach batch request creation
-    pkg.BatchRequest = requestManager.BatchManager.bind(null, pkg._requestManager);
+    this.BatchRequest = BatchManager.bind(null, this._requestManager);
 
     // attach extend function
-    pkg.extend = extend(pkg);
+    this.extend = extend(this);
+  }
+
+  setRequestManager (manager) {
+    this._requestManager = manager;
+    this._provider = manager.provider;
+  }
+
+  set curentProvider (newProvider) {
+    return this.setProvider(newProvider);
+  }
+
+  get curentProvider () {
+    return this._provider;
+  }
+
+  setProvider (provider, net) {
+    this._requestManager.setProvider(provider, net);
+    this._provider = this._requestManager.provider;
+    this.extend = extend(this);
+    return true;
+  }
+
+  addProviders (pkg) {
+    pkg.givenProvider = Manager.givenProvider;
+    pkg.providers = Manager.providers;
+  }
 }
 
-const addProviders = (pkg) => {
-    pkg.givenProvider = requestManager.Manager.givenProvider;
-    pkg.providers = requestManager.Manager.providers;
-}
-
-module.exports = {
-    packageInit,
-    addProviders
-}
+module.exports = Core
